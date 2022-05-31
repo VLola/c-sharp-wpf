cfb8bb927ba41ee2ee2197b95867fb35bb192e06
Valentin <valiklolanew@gmail.com>
2022-05-31 19:29:44 +0300

    added project 54

---
+++ Project_54/Controls/ImageControl.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Project_54.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_54.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для ImageControl.xaml
+    /// </summary>
+    public partial class ImageControl : UserControl
+    {
+        public Logo_Model logo_model = new Logo_Model();
+        public ImageControl()
+        {
+            InitializeComponent();
+            this.DataContext = logo_model;
+        }
+
+        private void But_1_Click(object sender, RoutedEventArgs e)
+        {
+            logo_model.number_logo = 0;
+        }
+        private void But_2_Click(object sender, RoutedEventArgs e)
+        {
+            logo_model.number_logo = 1;
+        }
+        private void But_3_Click(object sender, RoutedEventArgs e)
+        {
+            logo_model.number_logo = 2;
+        }
+        private void But_4_Click(object sender, RoutedEventArgs e)
+        {
+            logo_model.number_logo = 3;
+        }
+        private void But_5_Click(object sender, RoutedEventArgs e)
+        {
+            logo_model.number_logo = 4;
+        }
+        private void But_6_Click(object sender, RoutedEventArgs e)
+        {
+            logo_model.number_logo = 5;
+        }
+
+        private void Left_Click(object sender, RoutedEventArgs e)
+        {
+            logo_model.number_logo--;
+            if (logo_model.number_logo < 0) logo_model.number_logo = 5;
+        }
+        private void Rigth_Click(object sender, RoutedEventArgs e)
+        {
+            logo_model.number_logo++;
+            if (logo_model.number_logo > 5) logo_model.number_logo = 0;
+        }
+    }
+}
---
+++ Project_54/Objects/Logo_Model.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+
+namespace Project_54.Objects
+{
+    public class Logo_Model : INotifyPropertyChanged
+    {
+        private int _number_logo;
+        public int number_logo
+        {
+            get { return _number_logo; }
+            set
+            {
+                _number_logo = value;
+                OnPropertyChanged("number_logo");
+            }
+        }
+        public Logo_Model()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
