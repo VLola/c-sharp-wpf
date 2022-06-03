a10302986c256a7f75202c79ba934699eed0ad96
Valentin <valiklolanew@gmail.com>
2022-06-03 03:01:04 +0300

    added product

--- Project_54/Control/ProductControl.xaml.cs
+++ Project_54/Control/ProductControl.xaml.cs
@@ -1,28 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Project_54.Control
 {
-    /// <summary>
-    /// Логика взаимодействия для ProductControl.xaml
-    /// </summary>
     public partial class ProductControl : UserControl
     {
+        public ImageSource logo_one { get; set; }
+        public ImageSource logo_two { get; set; }
+        public string name { get; set; }
+        public string review { get; set; }
+        public string price { get; set; }
+        public string discount { get; set; }
         public ProductControl()
         {
             InitializeComponent();
+            this.DataContext = this;
         }
     }
 }
