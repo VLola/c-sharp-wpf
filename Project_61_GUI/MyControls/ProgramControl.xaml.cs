a22d110b4ff2a660f30d899752c53e9ccf5f179d
Valentin <valiklolanew@gmail.com>
2022-07-22 20:14:08 +0300

    added my Model and my Control

---
+++ Project_61_GUI/MyControls/ProgramControl.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Project_61_GUI.MyModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_61_GUI.MyControls
+{
+    /// <summary>
+    /// Логика взаимодействия для ProgramControl.xaml
+    /// </summary>
+    public partial class ProgramControl : UserControl
+    {
+        AppDomain Domain = AppDomain.CurrentDomain;
+        public MyProgram myProgram { get; set; }
+        public ProgramControl(ref MyProgram myProgram)
+        {
+            this.myProgram = myProgram;
+            InitializeComponent();
+            this.DataContext = this;
+            myProgram.PropertyChanged += MyProgram_PropertyChanged;
+        }
+
+        private void MyProgram_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "TimeControl")
+            {
+                Domain.SetData("GUI TimeControl:" + myProgram.ProgramName, myProgram.TimeControl);
+            }
+        }
+    }
+}
---
+++ Project_61_GUI/MyModels/MyProgram.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Project_61_GUI.MyModels
+{
+    public class MyProgram : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        private string _ProgramName { get; set; }
+        public string ProgramName
+        {
+            get { return _ProgramName; }
+            set
+            {
+                _ProgramName = value;
+                OnPropertyChanged("ProgramName");
+            }
+        }
+        private string _FullName { get; set; }
+        public string FullName
+        {
+            get { return _FullName; }
+            set
+            {
+                _FullName = value;
+                OnPropertyChanged("FullName");
+            }
+        }
+        private TimeSpan _WorkingTime { get; set; }
+        public TimeSpan WorkingTime
+        {
+            get { return _WorkingTime; }
+            set
+            {
+                _WorkingTime = value;
+                OnPropertyChanged("WorkingTime");
+            }
+        }
+        private bool _TimeControl { get; set; }
+        public bool TimeControl
+        {
+            get { return _TimeControl; }
+            set
+            {
+                _TimeControl = value;
+                OnPropertyChanged("TimeControl");
+            }
+        }
+    }
+}
