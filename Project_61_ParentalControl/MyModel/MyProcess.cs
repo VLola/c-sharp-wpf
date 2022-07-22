f46a71e88ad81ba58871c57afbb296821d5e9f0e
Valentin <valiklolanew@gmail.com>
2022-07-22 19:57:12 +0300

    added models

---
+++ Project_61_ParentalControl/MyModel/MyProcess.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Project_61_ParentalControl.MyModel
+{
+    public class MyProcess
+    {
+        private AppDomain _domain;
+        private TimeSpan _oneSecond = TimeSpan.FromSeconds(1);
+        public Variables Variables { get; set; } = new Variables();
+        public MyProcess(string Name, string FullName, DateTime StartTime, AppDomain Domain)
+        {
+            _domain = Domain;
+            Variables.ProgramName = Name;
+            Variables.FullName = FullName;
+            Variables.WorkingTime = DateTime.Now - StartTime;
+            Variables.SelectedWorkingTime = 720;
+            RunAsync();
+        }
+
+        private async void RunAsync()
+        {
+            await Task.Run(async () => {
+                while (true)
+                {
+                    if(_domain != null)
+                    {
+                        bool? check = (bool?)_domain.GetData("GUI TimeControl:" + Variables.ProgramName);
+                        if (check != null) Variables.TimeControl = (bool)check;
+                    }
+                    
+                    if (Variables.isWorkingProcess) Variables.WorkingTime += _oneSecond;
+                    if (Variables.ParentalControl && Variables.WorkingTime.TotalMinutes >= Variables.SelectedWorkingTime) ProcessKillAsync(Variables.ProgramName);
+                    TimeControlAsync();
+                    if (_domain != null) SetDataAsync();
+                    await Task.Delay(1000);
+                }
+            });
+        }
+        private async void SetDataAsync()
+        {
+            await Task.Run(()=> {
+                _domain.SetData("WorkingTime:" + Variables.ProgramName, Variables.WorkingTime);
+                _domain.SetData("FullName:" + Variables.ProgramName, Variables.FullName);
+                _domain.SetData("TimeControl:" + Variables.ProgramName, Variables.TimeControl);
+            });
+        }
+        private async void ProcessKillAsync(string name)
+        {
+            await Task.Run(() => {
+                foreach (Process item in Process.GetProcesses()) if (item.ProcessName == name) item.Kill();
+            });
+        }
+
+        private async void TimeControlAsync()
+        {
+            await Task.Run(() =>
+            {
+                bool Working = false;
+                foreach (var it in Process.GetProcesses()) if (it.ProcessName == Variables.ProgramName) Working = true;
+                if (Working) Variables.isWorkingProcess = true;
+                else Variables.isWorkingProcess = false;
+            });
+        }
+    }
+}
---
+++ Project_61_ParentalControl/MyModel/Variables.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Project_61_ParentalControl.MyModel
+{
+    public class Variables : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private bool _isLogin;
+        public bool isLogin
+        {
+            get { return _isLogin; }
+            set
+            {
+                _isLogin = value;
+                OnPropertyChanged("isLogin");
+            }
+        }
+
+        private string _ProgramName { get; set; }
+        public string ProgramName
+        {
+            get { return _ProgramName; }
+            set
+            {
+                _ProgramName = value;
+                OnPropertyChanged("ProgramName");
+            }
+        }
+        private string _FullName { get; set; }
+        public string FullName
+        {
+            get { return _FullName; }
+            set
+            {
+                _FullName = value;
+                OnPropertyChanged("FullName");
+            }
+        }
+        private TimeSpan _WorkingTime { get; set; }
+        public TimeSpan WorkingTime
+        {
+            get { return _WorkingTime; }
+            set
+            {
+                _WorkingTime = value;
+                OnPropertyChanged("WorkingTime");
+            }
+        }
+        private double _SelectedWorkingTime { get; set; }
+        public double SelectedWorkingTime
+        {
+            get { return _SelectedWorkingTime; }
+            set
+            {
+                _SelectedWorkingTime = value;
+                OnPropertyChanged("SelectedWorkingTime");
+            }
+        }
+        private bool _Finish { get; set; }
+        public bool Finish
+        {
+            get { return _Finish; }
+            set
+            {
+                _Finish = value;
+                OnPropertyChanged("Finish");
+            }
+        }
+        private bool _ParentalControl { get; set; }
+        public bool ParentalControl
+        {
+            get { return _ParentalControl; }
+            set
+            {
+                _ParentalControl = value;
+                OnPropertyChanged("ParentalControl");
+            }
+        }
+        private bool _TimeControl { get; set; }
+        public bool TimeControl
+        {
+            get { return _TimeControl; }
+            set
+            {
+                _TimeControl = value;
+                OnPropertyChanged("TimeControl");
+            }
+        }
+        private bool _isWorkingProcess = true;
+        public bool isWorkingProcess
+        {
+            get { return _isWorkingProcess; }
+            set
+            {
+                _isWorkingProcess = value;
+                OnPropertyChanged("isWorkingProcess");
+            }
+        }
+    }
+}
